8c3907ca64e65b6189a2f8751c5bcb6837ca1dff
Prakash Prajapati <prakash398prajapati@gmail.com>
2026-08-16 15:05:52 +0530

    add code

---
+++ symbol/index.js
@@ -0,0 +1,53 @@
+
+// *** creating Symbol *******
+
+// // const sym = Symbol("Hello Bother");
+// // console.log(sym);
+
+// // const obj = {};
+// // obj[sym]= "Prakash"
+
+// //  console.log(obj[sym])
+
+// const name = Symbol();
+
+//  const person = {
+//     [name]:"Prakash",
+//     age:20
+//  }
+
+//  for( const prop in person)
+//  {
+//      console.log(prop)
+//  }
+
+//   console.log(Object.keys(person))
+//   console.log(Object.getOwnPropertyNames(person))
+
+//   console.log(Object.getOwnPropertySymbols(person));
+
+// // console.log(person.name);
+// // console.log(person[name]);
+
+
+
+
+//****** Description add in symbol *******
+
+// const propSymbol = Symbol("property symbol");
+//  console.log(propSymbol.description)
+
+
+
+
+
+// ***** Global symbol *****
+
+// const globalSymbolKey = "my-prakash-symbol";
+// const newSymbol = Symbol.for(globalSymbolKey);
+
+// console.log(newSymbol === Symbol.for(globalSymbolKey));
+// console.log(newSymbol);
+
+
+
---
+++ this/index.js
@@ -0,0 +1,6 @@
+"use strict"
+
+function PrintData ()
+{
+     console.log(`This is My First ${this.name}`)
+}
